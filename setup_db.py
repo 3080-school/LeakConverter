6c187de09cb97295d81206976dd3f3c1b7ebecb2
Taimas300 <salimayazhan@MacBook-Air--Taimas.local>
2025-07-25 08:55:29 +0500

    Add script to clear users table and add user

--- setup_db.py
+++ setup_db.py
@@ -3,10 +3,35 @@ import sqlite3
 conn = sqlite3.connect("users.db")
 cursor = conn.cursor()
 
-
-# Добавим тестового пользователя
-cursor.execute("INSERT OR IGNORE INTO users (username, password) VALUES (?, ?)", ("0003", "arbuz3"))
-
+# ОЧИСТИТЬ ТАБЛИЦУ users
+cursor.execute("DELETE FROM answers")
 conn.commit()
+
+# Если надо добавить пользователя после очистки:
+cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", ("0004", "arbuz4"))
+conn.commit()
+# Если надо добавить пользователя после очистки:
+cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", ("0005", "arbuz5"))
+conn.commit()
+# Если надо добавить пользователя после очистки:
+cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", ("0006", "arbuz6"))
+conn.commit()
+# Если надо добавить пользователя после очистки:
+cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", ("0007", "arbuz7"))
+conn.commit()
+# Если надо добавить пользователя после очистки:
+cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", ("0008", "arbuz8"))
+conn.commit()
+# Если надо добавить пользователя после очистки:
+cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", ("0009", "arbuz9"))
+conn.commit()
+# Если надо добавить пользователя после очистки:
+cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", ("0010", "arbuz10"))
+conn.commit()
+# Если надо добавить пользователя после очистки:
+cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", ("0011", "arbuz11"))
+conn.commit()
+
+
 conn.close()
 print("success.")
